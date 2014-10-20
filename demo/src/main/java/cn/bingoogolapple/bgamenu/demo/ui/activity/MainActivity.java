e6ec84977c6a63dbee123ba9c34f6152e39c625b
bingoogolapple <bingoogolapple@gmail.com>
2014-10-20 20:18:27 +0800

    Initial commit

---
+++ demo/src/main/java/cn/bingoogolapple/bgamenu/demo/ui/activity/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.bingoogolapple.bgamenu.demo.ui.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import cn.bingoogolapple.bgamenu.demo.R;
+import cn.bingoogolapple.loon.library.LoonLayout;
+
+
+@LoonLayout(id = R.layout.activity_main)
+public class MainActivity extends FragmentActivity {
+
+}
