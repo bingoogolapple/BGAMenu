220b2ceea085c4676b4fb7ad9aea71e9a81c3a5d
bingoogolapple <bingoogolapple@gmail.com>
2014-10-21 15:28:04 +0800

    :feet:

--- demo/src/main/java/cn/bingoogolapple/bgamenu/demo/ui/activity/MainActivity.java
+++ demo/src/main/java/cn/bingoogolapple/bgamenu/demo/ui/activity/MainActivity.java
@@ -1,21 +1,23 @@
 package cn.bingoogolapple.bgamenu.demo.ui.activity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
-import cn.bingoogolapple.bgamenu.library.BGAMenu;
 import cn.bingoogolapple.bgamenu.demo.R;
+import cn.bingoogolapple.bgamenu.library.BGAMenu;
 import cn.bingoogolapple.loon.library.Loon;
 import cn.bingoogolapple.loon.library.LoonLayout;
 import cn.bingoogolapple.loon.library.LoonView;
 
-
 @LoonLayout(id = R.layout.activity_main)
 public class MainActivity extends FragmentActivity {
-    @LoonView(id = R.id.menu)
-    private BGAMenu mMenu;
+    @LoonView(id = R.id.menu1)
+    private BGAMenu mMenu1;
+    @LoonView(id = R.id.menu2)
+    private BGAMenu mMenu2;
+    @LoonView(id = R.id.menu3)
+    private BGAMenu mMenu3;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +25,15 @@ public class MainActivity extends FragmentActivity {
         Loon.injectView2Activity(this);
     }
 
-    public void toggleMenu(View view) {
-        mMenu.toggle();
+    public void toggleMenu1(View view) {
+        mMenu1.toggle();
+    }
+
+    public void toggleMenu2(View view) {
+        mMenu2.toggle();
+    }
+
+    public void toggleMenu3(View view) {
+        mMenu3.toggle();
     }
 }
